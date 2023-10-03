d2828e2b65ce4060e9eb908db61fc06111f6e67a
FardsyadCS270UITM <2020601972@student.uitm.edu.my>
2023-10-03 16:04:42 +0800

    First commit

---
+++ lib/home.dart
@@ -0,0 +1,167 @@
+import 'package:tflite/tflite.dart';
+
+import 'main.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:get/get.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  bool isWorking = false;
+  String result = '';
+  CameraController? cameraController;
+  CameraImage? imgCamera;
+
+  initCamera()
+  {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value)
+    {
+      if(!mounted)
+        {
+          return;
+        }
+      setState(() {
+        cameraController!.startImageStream((imageFromStream) =>
+        {
+          if(!isWorking)
+            {
+              isWorking = true,
+              imgCamera = imageFromStream,
+              runModelOnStreamFrames(),
+            }
+        });
+      });
+    });
+  }
+//error guna tflite
+  loadModel() async{
+    await Tflite.loadModel(
+      model:"assets/model_unquant.tflite",
+      labels:"assets/labels.txt",
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadModel();
+  }
+
+  runModelOnStreamFrames()async{
+    if(imgCamera != null)
+      {
+        var recognition = await Tflite.runModelOnFrame(
+            bytesList: imgCamera!.planes.map((plane)
+            {
+              return plane.bytes;
+            }).toList(),
+
+          imageHeight: imgCamera!.height,
+          imageWidth: imgCamera!.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          rotation: 90,
+          numResults: 2,
+          threshold: 0.1,
+          asynch: true,
+        );
+        result = '';
+
+        recognition!.forEach((response)
+        {
+          result += response["label"] + "" + (response["confidence"] as double).toStringAsFixed(2) + "\n\n";
+        });
+        setState(() {
+          result;
+        });
+        isWorking = false;
+      }
+  }
+
+  @override
+  void dispose() async{
+    // TODO: implement dispose
+    super.dispose();
+
+    await Tflite.close();
+    cameraController?.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(title: Text("Fish Identifier"),),
+          body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(""),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+
+
+                    Center(
+                      child: TextButton(
+                        onPressed: ()
+                        {
+                          initCamera();
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(top: 35.0),
+                          height: 270.0,
+                          width: 360.0,
+                          child: imgCamera == null
+                            ?
+                              Container(
+                                height: 270.0,
+                                width: 360.0,
+                                child: Icon(Icons.photo_camera_front,color: Colors.pink,size: 40.0,),
+                              )
+                              :
+                              AspectRatio(
+                                aspectRatio: cameraController!.value.aspectRatio,
+                                child: CameraPreview(cameraController!),
+                              ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 55.0),
+                    child: SingleChildScrollView(
+                      child: Text(
+                        result,
+                        style: const TextStyle(
+                          backgroundColor: Colors.white54,
+                          fontSize: 25.0,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
